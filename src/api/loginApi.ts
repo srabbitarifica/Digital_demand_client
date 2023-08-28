d0518586d0f8f9f909aea1a270f9c9c04b8393e6
srabbitarifica <92047349+srabbitarifica@users.noreply.github.com>
2023-08-28 21:25:57 +0800

    Update loginApi.ts

--- src/api/loginApi.ts
+++ src/api/loginApi.ts
@@ -11,7 +11,7 @@ export const loginApi = (
   console.log("API URL:", apiUrl); // Log the API URL
 
   return axios
-    .post(`${process.env.NEXT_PUBLIC_API_URL}login` , {
+    .post("https://servertemp.azurewebsites.net/login" , {
       username,
       password,
     })
