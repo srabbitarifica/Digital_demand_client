7cd5161c7a38cd71ed30f079b7e900fb51177d66
srabbitarifica <92047349+srabbitarifica@users.noreply.github.com>
2023-09-08 13:39:58 -0400

    Update getData.ts

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 export const getData = (loading: (value: boolean) => void) => {
 
-  //const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}data`;
-
-  console.log("API URL:", apiUrl); // Log the API URL
   return axios
     // .get(`${process.env.NEXT_PUBLIC_API_URL}data`)
     .get("https://serverd.azurewebsites.net/data")
