9a614d353419f3b9658a475bcab448bca4cb0e38
srabbitarifica <92047349+srabbitarifica@users.noreply.github.com>
2023-09-12 21:08:07 +0800

    Update getData.ts

--- src/api/getData.ts
+++ src/api/getData.ts
@@ -4,7 +4,8 @@ export const getData = (loading: (value: boolean) => void) => {
 
   return axios
     // .get(`${process.env.NEXT_PUBLIC_API_URL}data`)
-    .get("https://serverd.azurewebsites.net/data")
+    // .get("https://serverd.azurewebsites.net/data")
+    .get("https://serverdashboardtest.azurewebsites.net/data")
     .then((response) => {
       loading(true);
       return response.data;
